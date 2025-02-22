28efb22abf4731bd9ed0d8a54dde9f01a4516ecb
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-02-22 10:59:32 +0530

    [Harsh]add. Make REST Call to show Hello Mark from BridgeLabz Using GET Request Method and pass name as path variable.

--- hello/HelloController.java
+++ hello/HelloController.java
@@ -1,9 +1,6 @@
 package com.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -15,5 +12,10 @@ public class HelloController {
     @GetMapping("/UC2")
     public String sayHelloWithName(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/UC3/{name}")
+    public String sayHelloWithPath(@PathVariable String name) {
+        return  "Hello " + name + " from BridgeLabz";
     }
 }
